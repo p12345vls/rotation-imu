8ba15ca52754b0ab15bb2b11f2b8d62ccc15cb7d
p12345vls <p123245vls@gmail.com>
2020-07-31 15:39:24 -0700

    added rotation

---
+++ Assets/Distance.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+
+
+public class Distance : MonoBehaviour
+{
+    SerialPort stream2 = new SerialPort("COM4", 115200);
+    public string range;
+    
+   
+
+    /*public Vector3 rotationAngles;*/
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stream2.Open();
+    
+
+    }
+
+
+ /*   public void eulerRotation(Vector3 v)
+    {
+        rotationAngles = v;
+    }*/
+
+    // Update is called once per frame
+    void Update()
+    {
+        range = stream2.ReadLine();
+        stream2.BaseStream.Flush(); //Clear the serial information so we assure we get new information.
+
+        /*GameObject.Find("Rotation").SendMessage("getDistance", range);*/
+
+        /* float degrX = rotationAngles.x;
+         float degrY = rotationAngles.y;
+         float degrZ = rotationAngles.z;
+
+         float radX = degrX * Mathf.Deg2Rad;
+         float radY = degrY * Mathf.Deg2Rad;
+         float radZ = degrZ * Mathf.Deg2Rad;
+
+         float cosx = (float)Mathf.Cos(radX);
+         float cosy = (float)Mathf.Cos(radY);
+         float cosz = (float)Mathf.Cos(radZ);
+
+
+         float laserDistance = (float.Parse(receivedLaser) / 10) - 1.0f;
+
+
+         transform.position = Vector3.Lerp(transform.position,
+         //new Vector3(0, 0, (float.Parse(receivedLaser) / 10) - 1.0f), 5.0f * Time.deltaTime);
+         new Vector3(laserDistance * cosx, laserDistance * cosy, laserDistance * cosz), 5.0f * Time.deltaTime);*/
+
+
+    }
+}
--- Assets/rotation.cs
+++ Assets/rotation.cs
@@ -4,42 +4,66 @@ using UnityEngine;
 using System.IO.Ports;
 using System.Threading;
 using System.Configuration;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 public class Rotation : MonoBehaviour
 {
 
-    SerialPort serial = new SerialPort("COM3",115200);
+    SerialPort serial = new SerialPort("COM3", 115200);
+    public float millimeters;
+    public GameObject backSense;
 
     // Start is called before the first frame update
     void Start()
     {
-        serial.Open();
+       backSense  = GameObject.Find("ReceiverRotation");
+       serial.Open();
+    }
+
+    void getDistance(float range)
+    {
+        millimeters = range;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //print(millimeters);
 
-        string data = serial.ReadLine();
-        
-        try
+        if (serial.IsOpen)
         {
+            string data = serial.ReadLine();
 
-            string[] values = data.Split('/');
+            try
+            {
 
-            float w = float.Parse(values[0]);
-            float x = float.Parse(values[1]);
-            float y = float.Parse(values[2]);
-            float z = float.Parse(values[3]);
+                string[] values = data.Split('/');
 
-            this.transform.localRotation = Quaternion.Lerp(this.transform.localRotation,
-                new Quaternion(w, x, y, z), Time.deltaTime * 15.0f);
+                if (values.Length == 4)
+                {
+                    float w = float.Parse(values[0]);
+                    float x = float.Parse(values[1]);
+                    float y = float.Parse(values[2]);
+                    float z = float.Parse(values[3]);
+
+
+                    backSense.transform.localRotation = Quaternion.Lerp(backSense.transform.localRotation,
+                        new Quaternion(w, x, y, z), Time.deltaTime * 15.0f);
+
+                }
+
+
+                //print(this.transform.eulerAngles.x+" "+ this.transform.eulerAngles.y + " "+ this.transform.eulerAngles.z);
+
+            }
+            catch (System.FormatException e)
+            {
+                print(e.Message);
+            }
+
 
         }
-        catch (System.FormatException e)
-        {
-            print(e.Message);
-        }
-        
+
     }
 }
